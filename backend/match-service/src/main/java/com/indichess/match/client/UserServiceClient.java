3910f6e8b886014cb40e5fe05273887e28eb9b10
Vikas Reddy <kathulavikasr@gmail.com>
2026-02-06 15:04:17 +0530

    feat: Add auth-service and Eureka service for discovery, replacing the config service.

--- backend/match-service/src/main/java/com/indichess/match/client/UserServiceClient.java
+++ backend/match-service/src/main/java/com/indichess/match/client/UserServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${feign.user-service.url:http://localhost:8081}")
+@FeignClient(name = "user-service")
 public interface UserServiceClient {
 
     @GetMapping("/api/users/{userId}")
